4c456939c1ebdf6edd4deb2ecd5d58ad211ab384
Len Heekhout <len.heekhout@student.hogent.be>
2026-04-30 14:28:51 +0200

    Titel aangepast

--- SerialCommunication/Form1.Designer.cs
+++ SerialCommunication/Form1.Designer.cs
@@ -875,7 +875,7 @@
             this.Controls.Add(this.comboBoxPoort);
             this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
-            this.Text = "BZL seriële communicatie";
+            this.Text = "BZL seriële communicatie Len Heekhout";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl.ResumeLayout(false);
             this.tabPageInstellingen.ResumeLayout(false);
